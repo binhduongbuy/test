ab04c8ba43d0c6f7cdbf6d810f7598fdef059509
Truong Dung <34378419+binhduongbuy@users.noreply.github.com>
2018-07-21 00:14:10 +0700

    1

---
+++ components/SideBar/index.jsx
@@ -0,0 +1,43 @@
+import React from 'react'
+import Youtube from '../Youtube'
+
+import './style.scss'
+
+class SideBar extends React.Component {
+    render() {
+
+    const AdsenseSponsorAds = buildAdsenseSponsorAds();
+    const AdsenseSponsorAdsEnds = buildAdsenseSponsorAdsEnds();
+
+        return (
+            
+        );
+    }
+}
+
+function buildAdsenseSponsorAds() {
+  const js = `
+  <!-- Sponsor Right Sidebar -->
+<ins class='adsbygoogle'
+     style='display:block'
+     data-ad-client='ca-pub-IDHERE'
+     data-ad-slot='8665159676'
+     data-ad-format='auto'></ins>
+  `;
+
+  return <script
+    dangerouslySetInnerHTML={{ __html: js }}
+  />;
+}
+
+function buildAdsenseSponsorAdsEnds() {
+  const js = `
+  (adsbygoogle = window.adsbygoogle || []).push({});
+  `;
+
+  return <script
+    dangerouslySetInnerHTML={{ __html: js }}
+  />;
+}
+
+export default SideBar
